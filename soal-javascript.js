822fdaa51ffbf9632c6b4811cd8de52618c3aa0e
Ari P <ariteknologi@gmail.com>
2022-03-19 03:27:58 +0700

    initial commit

---
+++ soal-javascript.js
@@ -0,0 +1,120 @@
+function fizzBuzz(n)
+{
+    let string = ""
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 == 0) string += "FizzBuzz";
+        else if (i % 3 == 0) string += "Fizz";
+        else if (i % 5 == 0) string += "Buzz";
+        else string +=i;
+        string += "\n"
+    }
+    return string;
+}
+
+console.log(fizzBuzz(100))
+
+function currency(n) {
+
+    let string = n.toString(),
+        units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
+
+    if (parseInt(string) === 0) {
+        return 'zero';
+    }
+
+    /* Array of units as words */
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    /* Array of tens as words */
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    /* Array of scales as words */
+    scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'];
+
+    /* Split user arguemnt into 3 digit chunks from right to left */
+    start = string.length;
+    chunks = [];
+    while (start > 0) {
+        end = start;
+        chunks.push(string.slice((start = Math.max(0, start - 3)), end));
+    }
+
+    /* Check if function has enough scale words to be able to stringify the user argument */
+    chunksLen = chunks.length;
+    if (chunksLen > scales.length) {
+        return '';
+    }
+
+    /* Stringify each integer in each chunk */
+    words = [];
+    for (i = 0; i < chunksLen; i++) {
+
+        chunk = parseInt(chunks[i]);
+
+        if (chunk) {
+
+            /* Split chunk into array of individual integers */
+            ints = chunks[i].split('').reverse().map(parseFloat);
+
+            /* If tens integer is 1, i.e. 10, then add 10 to units integer */
+            if (ints[1] === 1) {
+                ints[0] += 10;
+            }
+
+            /* Add scale word if chunk is not zero and array item exists */
+            if ((word = scales[i])) {
+                words.push(word);
+            }
+
+            /* Add unit word if array item exists */
+            if ((word = units[ints[0]])) {
+                words.push(word);
+            }
+
+            /* Add tens word if array item exists */
+            if ((word = tens[ints[1]])) {
+                words.push(word);
+            }
+
+            /* Add hundreds word if array item exists */
+            if ((word = units[ints[2]])) {
+                words.push(word + ' hundred');
+            }
+
+        }
+
+    }
+
+    return words.reverse().join(' ');
+
+}
+
+console.log(currency(123456))
+
+function multiply(a, b) {
+    let result = a;
+    for (let i = 1; i < b; i++) {
+        result += a
+    }
+    
+    return result;
+}
+
+console.log(multiply(6,5))
+
+
+function fibonacci(number) {
+    let n1 = 0, n2 = 1, nextTerm, arr = [];
+    
+    for (let i = 1; i <= number; i++) {
+        arr.push(n1)
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+
+    return arr.toString(" ")
+}
+
+
+console.log(fibonacci(10))
